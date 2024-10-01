c27ae22f01f703c14cdab71bf90f893f412d1336
Emmanuella <princess01ella.miley@gmail.com>
2024-10-01 16:45:10 +0200

    feat: load more

--- client/src/repository.tsx
+++ client/src/repository.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { TextField, Button, Container, List, ListItem, Typography, Checkbox, IconButton } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { fetchRepositories, storeExpanded } from './services/api';
+import React, { useState } from "react";
+import { TextField, Button, Container, List, ListItem, Typography, Checkbox, IconButton } from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { fetchRepositories, storeExpanded } from "./services/api";
 
 interface branchesResponse {
     name: string;
-    commit: { sha: string; url: string; };
-    protected: boolean
+    commit: { sha: string; url: string };
+    protected: boolean;
 }
 
 interface Repo {
@@ -14,97 +14,111 @@ interface Repo {
     name: string;
     url: string;
     language: string;
-    total_branches: number
+    total_branches: number;
     branches: branchesResponse[];
     expanded: boolean;
 }
 
 function App() {
-    const [org, setOrg] = useState('');
-    const [page, setPage] = useState(0);
-    //   const [hasMore, setHasMore] = useState(true);
+    const [org, setOrg] = useState("");
+    const [page, setPage] = useState(1);
+    const [more, setMore] = useState(true);
     const [repos, setRepos] = useState<Repo[]>([]);
     const [expandedRepos, setExpandedRepos] = useState<number[]>([]);
 
     const fetchRepos = async () => {
         try {
-            if (page > 0) return;
-            setPage(1);
             const data: Repo[] = await fetchRepositories(org, page);
-            setRepos((prev) => [...prev, ...data]);
+            setRepos(prev => [...prev, ...data]);
+            setMore(true)
+
             // set previously expanded repos
             data.map(repo => {
                 if (repo.expanded) {
                     toggleExpand(repo.id);
                 }
                 return repo;
-            })
-            //   if (data.length === 0) setHasMore(false);
+            });
+
+            if (data.length === 0) setMore(false);
         } catch (error) {
-            console.error('Error fetching repositories:', error);
-            //   setHasMore(false)
+            console.error("Error fetching repositories:", error);
+            setMore(false);
         }
     };
 
     const toggleExpand = async (id: number, org?: string) => {
         let expanded = true;
-        setExpandedRepos((prev) => {
+        setExpandedRepos(prev => {
             // If the id is in the array, remove it (collapse)
             if (prev.includes(id)) {
-                expanded = false
-                return prev.filter((i) => i !== id);
+                expanded = false;
+                return prev.filter(i => i !== id);
             }
             // If the id is not in the array, add it (expand)
             return [...prev, id];
         });
 
         if (org) {
-            await storeExpanded(org, id, expanded)
+            await storeExpanded(org, id, expanded);
         }
     };
 
     return (
         <Container>
-
             <TextField
                 label="GitHub Organization"
                 variant="outlined"
                 value={org}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    e.preventDefault()
-                    setPage(0)
-                    setRepos([])
-                    setExpandedRepos([])
-                    setOrg(e.target.value)
+                    e.preventDefault();
+                    setPage(1);
+                    setRepos([]);
+                    setExpandedRepos([]);
+                    setMore(false)
+                    setOrg(e.target.value);
                 }}
                 fullWidth
             />
 
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={fetchRepos}
-                style={{ marginTop: '16px', marginBottom: '16px' }}
-            >
-                Search
-            </Button>
+            {page == 1 && (
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={async () => {
+                        await fetchRepos();
+                        setPage(page => page + 1);
+                    }}
+                    style={{ marginTop: "16px", marginBottom: "16px" }}
+                >
+                    Search
+                </Button>
+            )}
 
             <List>
-                {repos.map((repo) => (
+                {repos.map(repo => (
                     <ListItem key={repo.id}>
                         <Checkbox />
-                        <Typography variant="body1" style={{ flex: 1 }}>{repo.name}</Typography>
-                        <Typography variant="body2" style={{ flex: 1 }}><a href={repo.url}> Repo Link</a></Typography>
-                        <Typography variant="body2" style={{ flex: 1 }}>Language: {repo.language} </Typography>
-                        <Typography variant="body2" style={{ flex: 1 }}>Branches: {repo.total_branches}</Typography>
+                        <Typography variant="body1" style={{ flex: 1 }}>
+                            {repo.name}
+                        </Typography>
+                        <Typography variant="body2" style={{ flex: 1 }}>
+                            <a href={repo.url}> Repo Link</a>
+                        </Typography>
+                        <Typography variant="body2" style={{ flex: 1 }}>
+                            Language: {repo.language}{" "}
+                        </Typography>
+                        <Typography variant="body2" style={{ flex: 1 }}>
+                            Branches: {repo.total_branches}
+                        </Typography>
                         <IconButton onClick={() => toggleExpand(repo.id, org)}>
                             <ExpandMoreIcon />
                         </IconButton>
 
                         {expandedRepos.includes(repo.id) && (
-                            <List style={{ marginBottom: '24px' }}>
+                            <List style={{ marginBottom: "24px" }}>
                                 {repo.branches.slice(0, 10).map((branch, i) => (
-                                    <ListItem key={i} style={{ display: 'block' }}>
+                                    <ListItem key={i} style={{ display: "block" }}>
                                         <div>
                                             {/* Branch name */}
                                             <Typography variant="body2">
@@ -112,8 +126,8 @@ function App() {
                                             </Typography>
 
                                             {/* Protected status */}
-                                            <Typography variant="body2" style={{ color: branch.protected ? 'green' : 'red' }}>
-                                                <strong>Status:</strong> {branch.protected ? 'Protected' : 'Unprotected'}
+                                            <Typography variant="body2" style={{ color: branch.protected ? "green" : "red" }}>
+                                                <strong>Status:</strong> {branch.protected ? "Protected" : "Unprotected"}
                                             </Typography>
                                         </div>
                                     </ListItem>
@@ -123,6 +137,16 @@ function App() {
                     </ListItem>
                 ))}
             </List>
+            {page > 0 && more && (
+                <button
+                    onClick={() => {
+                        setPage(page => page + 1);
+                        fetchRepos();
+                    }}
+                >
+                    Load more
+                </button>
+            )}
         </Container>
     );
 }
